f0c7ea34987325251a35da3733b8f0693aef9c29
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-04-19 16:55:16 +0530

    More code in Profile page

--- src/Pages/profile.js
+++ src/Pages/profile.js
@@ -1,9 +1,31 @@
-
+import React, { useContext ,useState,useEffect} from 'react';
+import {FirebaseContext} from '../context/firebase';
+import * as ROUTES from '../Routes_System/routes';
+import axios from 'axios';
+import {Loading} from '../components';
+import { SelectProfileContainer } from '../Container/profiles';
+import '../styles/profilePage.css';
 export default function Profile(){
+
+    const {firebase} =useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {};
+    const [profile,setProfile] = useState({});
+
     return(
         <>
-        <h1>Hello from profile</h1>
-        <h3> You get to see the profle</h3>
+        <div className ='form_class'>
+            <div className ="Incenter">
+                <h2>Your Profile</h2>
+                <div className="drag_area"><img src={`/images/users/${user.photoURL}.png`} alt ="Your Icon"/></div>
+
+                <div className="formInput">
+                    <h2>Name :</h2>
+                    <div className ="formChildren">
+                        {user.displayName}
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
