044e6adc25be9c6caa289e0c869b32b81aae6a92
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-05-29 00:12:28 +0530

    Cleaned files

--- src/Configurations/firebase.prod.js
+++ src/Configurations/firebase.prod.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 // import {sendDatabase} from '../database';
 
 const config ={
-    apiKey: "AIzaSyABL60xF8Ml1QlyUN4lcThXIB_LtysaT9M",
-    authDomain: "bingbox-f22d8.firebaseapp.com",
-    projectId: "bingbox-f22d8",
-    databaseURL: 'https://bingbox-f22d8.firebaseapp.com',
-    storageBucket: "bingbox-f22d8.appspot.com",
-    messagingSenderId: "8042230020",
-    appId: "1:8042230020:web:1333fa3305f682141da051"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    databaseURL: '',
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
